adffa98df8de7dec081aabbe3b8cd410842aa3b5
Faraazrehan2002 <faraazrehan2002@gmail.com>
2024-09-06 16:55:41 -0500

    Slider functionlity updated

--- WHR Calculator/ViewController.swift
+++ WHR Calculator/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var waistValueSlider: UISlider!
+    
+    
+    @IBOutlet weak var hipValueSlider: UISlider!
+    
+    @IBOutlet weak var waistLabelValue: UILabel!
+    
+    @IBOutlet weak var hipLabelValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func waistSliderChanged(_ sender: UISlider) {
+        
+        let waistValue = sender.value
+        waistLabelValue.text = String(format: "%1.1f", waistValue)
+        
+    }
+    
+    @IBAction func hipSliderChanged(_ sender: UISlider) {
+        
+        let hipValue = sender.value
+        hipLabelValue.text = String(format: "%1.1f", hipValue)
+        
+    }
+    
+    
+    @IBAction func WHRButtonCalculate(_ sender: Any) {
+    }
+    
 }
 
