67cfa4119ea52a5c436f735e79a582ca8cccef2f
Faraazrehan2002 <faraazrehan2002@gmail.com>
2024-09-06 16:17:25 -0500

    Initial Commit

---
+++ WHR Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WHR Calculator
+//
+//  Created by Faraaz Rehan Junaidi Mohammed on 9/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
